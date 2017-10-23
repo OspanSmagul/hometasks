4a5d5acefed748d220f5366372f95eca247856a9
OspanSmagul <smagulovospan@mail.ru>
2017-10-23 09:18:14 +0600

    Add Week6

---
+++ Week6/Task1.js
@@ -0,0 +1,25 @@
+function onText(){
+  if(document.getElementById('b1')){
+    document.getElementById('t1').setAttribute(
+   "style", "text-decoration-line:line-through");
+  }}
+function onText1(){
+  if(document.getElementById('b2')){
+    document.getElementById('t2').setAttribute(
+   "style", "text-decoration-line:line-through");
+  }}
+function onText2(){
+  if(document.getElementById('b3')){
+    document.getElementById('t3').setAttribute(
+   "style", "text-decoration-line:line-through");
+  }}
+function onText3(){
+  if(document.getElementById('b4')){
+    document.getElementById('t4').setAttribute(
+   "style", "text-decoration-line:line-through");
+  }}
+function onText4(){
+  if(document.getElementById('b5')){
+    document.getElementById('t5').setAttribute(
+   "style", "text-decoration-line:line-through");
+  }}
